482fda878145ca150711349391558679e42c0587
RSGmaker <rsraygamer@gmail.com>
2015-08-08 20:34:18 -0600

    renamed values to simpler terms

--- ComicLayouter/CropControl.Designer.cs
+++ ComicLayouter/CropControl.Designer.cs
@@ -48,22 +48,22 @@
             // 
             // trackBar1
             // 
-            this.trackBar1.Location = new System.Drawing.Point(12, 90);
+            this.trackBar1.Location = new System.Drawing.Point(36, 50);
             this.trackBar1.Maximum = 100;
             this.trackBar1.Minimum = 1;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(271, 45);
+            this.trackBar1.Size = new System.Drawing.Size(247, 45);
             this.trackBar1.TabIndex = 0;
             this.trackBar1.Value = 100;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // trackBar2
             // 
-            this.trackBar2.Location = new System.Drawing.Point(12, 128);
+            this.trackBar2.Location = new System.Drawing.Point(36, 155);
             this.trackBar2.Maximum = 100;
             this.trackBar2.Minimum = 1;
             this.trackBar2.Name = "trackBar2";
-            this.trackBar2.Size = new System.Drawing.Size(271, 45);
+            this.trackBar2.Size = new System.Drawing.Size(247, 45);
             this.trackBar2.TabIndex = 1;
             this.trackBar2.Value = 100;
             this.trackBar2.Scroll += new System.EventHandler(this.trackBar1_Scroll);
@@ -71,20 +71,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 100);
+            this.label1.Location = new System.Drawing.Point(4, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "X2";
+            this.label1.Text = "Right";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 138);
+            this.label2.Location = new System.Drawing.Point(0, 162);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.Size = new System.Drawing.Size(40, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Y2";
+            this.label2.Text = "Bottom";
             // 
             // pictureBox1
             // 
@@ -97,39 +97,39 @@
             // 
             // trackBar3
             // 
-            this.trackBar3.Location = new System.Drawing.Point(12, 7);
+            this.trackBar3.Location = new System.Drawing.Point(36, 7);
             this.trackBar3.Maximum = 100;
             this.trackBar3.Name = "trackBar3";
-            this.trackBar3.Size = new System.Drawing.Size(271, 45);
+            this.trackBar3.Size = new System.Drawing.Size(247, 45);
             this.trackBar3.TabIndex = 5;
             this.trackBar3.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // trackBar4
             // 
-            this.trackBar4.Location = new System.Drawing.Point(12, 39);
+            this.trackBar4.Location = new System.Drawing.Point(36, 112);
             this.trackBar4.Maximum = 100;
             this.trackBar4.Name = "trackBar4";
-            this.trackBar4.Size = new System.Drawing.Size(271, 45);
+            this.trackBar4.Size = new System.Drawing.Size(247, 45);
             this.trackBar4.TabIndex = 6;
             this.trackBar4.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(-2, 20);
+            this.label3.Location = new System.Drawing.Point(5, 21);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.Size = new System.Drawing.Size(25, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "X1";
+            this.label3.Text = "Left";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(-2, 55);
+            this.label4.Location = new System.Drawing.Point(4, 120);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.Size = new System.Drawing.Size(26, 13);
             this.label4.TabIndex = 8;
-            this.label4.Text = "Y1";
+            this.label4.Text = "Top";
             // 
             // CropControl
             // 
